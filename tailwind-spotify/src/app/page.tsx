add62a6507658f87af9882d20508e384efbee974
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-25 17:00:39 -0300

    feature

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -5,16 +5,22 @@ export default function Home() {
   <div className="h-screen flex flex-col">
     <div className="flex flex-1">
       <aside className="w-72 bg-zinc-950 p-6">
-        <nav>
-          <a href="" className="flex item-center gap-2 text-xs font-semibold text-zinc-100">
+        <div className="flex items-center gap-2">
+          <div className="w-3 h-3 bg-red-500 rounded-full"/>
+          <div className="w-3 h-3 bg-yellow-500 rounded-full"/>
+          <div className="w-3 h-3 bg-green-500 rounded-full"/>
+        </div>
+
+        <nav className="space-y-5 mt-10">
+          <a href="" className="flex item-center gap-3 text-sm font-semibold text-zinc-200">
             <HomeIcon />
             Home
           </a>
-          <a href="" className="flex item-center gap-2 text-xs font-semibold text-zinc-100">
+          <a href="" className="flex item-center gap-3 text-sm font-semibold text-zinc-200">
             <Search />
             Search
           </a>
-          <a href="" className="flex item-center gap-2 text-xs font-semibold text-zinc-100">
+          <a href="" className="flex item-center gap-3 text-sm font-semibold text-zinc-200">
             <Library />
             Your Library
           </a>
