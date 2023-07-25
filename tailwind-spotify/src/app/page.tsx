09d471b6b4979f0955304e367b6bd7f05ec02c41
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-25 18:33:26 -0300

    Design Updates

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Home as HomeIcon, Search, Library } from 'lucide-react';  
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react';  
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -25,9 +26,82 @@ export default function Home() {
             Your Library
           </a>
         </nav>
+
+        <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">My Playlist</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Mix Rock</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Hip Hop Controller</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Coding Mode</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Study lofi</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">No Repeat</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Mix Pop</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Coding lofi</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Good Vibes</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Rock</a>
+        </nav>
       </aside>  
       <main className="flex-1 p-6">
-        main
+        <div className="flex items-center gap-3">
+          <button className="rounded-full bg-black/40 p-1">
+            <ChevronLeft />
+          </button>
+          <button className="rounded-full bg-black/40 p-1">
+            <ChevronRight />
+          </button> 
+        </div>
+
+        <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+        <div className="grid grid-cols-3 gap-6 mt-4">
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <Image src="/album.jpg" width={104} height={104} alt='Capa do album Músicas Curtidas'/>
+            <strong>Músicas Curtidas</strong>
+
+            <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <Play />
+            </button>
+          </a>
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <strong>Starset</strong>
+
+            <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <Play />
+            </button>
+          </a>
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <strong>Skillet</strong>
+
+            <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <Play />
+            </button>
+          </a>
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <strong>Slipknot</strong>
+
+            <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <Play />
+            </button>
+          </a>
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <strong>Nirvana</strong>
+
+            <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <Play />
+            </button>
+          </a>
+          <a href="#" className="bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors">
+            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <strong>Study Lofi</strong> 
+
+             <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+              <Play />
+            </button>       
+          </a>
+        </div>
       </main>
     </div>
     <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
