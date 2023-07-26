af908068008f5b44a4b47651f334b48e6d74e124
Hanniel Vieira <hanielvieira1227@gmail.com>
2023-07-25 23:28:14 -0300

    Design updates

--- tailwind-spotify/src/app/page.tsx
+++ tailwind-spotify/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react';  
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, PlaySquare, Mic2, AlignJustify, MonitorSpeaker, Volume2, Maximize2 } from 'lucide-react';  
 import Image from 'next/image';
 
 export default function Home() {
@@ -62,7 +62,7 @@ export default function Home() {
             </button>
           </a>
           <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <Image src="/starset.jpg" width={104} height={104} alt='Capa do album músicas '/>
             <strong>Starset</strong>
 
             <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
@@ -70,7 +70,7 @@ export default function Home() {
             </button>
           </a>
           <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <Image src="/skillet_.jpg" width={104} height={104} alt='Capa do album músicas '/>
             <strong>Skillet</strong>
 
             <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
@@ -78,7 +78,7 @@ export default function Home() {
             </button>
           </a>
           <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <Image src="/slipknot.jpg" width={104} height={104} alt='Capa do album músicas '/>
             <strong>Slipknot</strong>
 
             <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
@@ -86,7 +86,7 @@ export default function Home() {
             </button>
           </a>
           <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <Image src="/nirvana.jpg" width={104} height={104} alt='Capa do album músicas '/>
             <strong>Nirvana</strong>
 
             <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
@@ -94,7 +94,7 @@ export default function Home() {
             </button>
           </a>
           <a href="#" className="bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors">
-            <Image src="/album.jpg" width={104} height={104} alt='Capa do album músicas '/>
+            <Image src="/lofi.jpg" width={104} height={104} alt='Capa do album músicas '/>
             <strong>Study Lofi</strong> 
 
              <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
@@ -134,16 +134,49 @@ export default function Home() {
         </div>
       </main>
     </div>
-    <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+    <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
       <div className="flex items-center gap-3">
-        <Image src="/album.jpg" width={56} height={56} alt='Capa do album Músicas Curtidas'/>
+        <Image src="/starset.jpg" width={56} height={56} alt='Capa do album Músicas Curtidas'/>
         <div className="flex flex-col">
           <strong className="text-normal">My Demons</strong>
           <span className="text-xs text-zinc-400">Starset</span>
         </div>
       </div>
-      <div></div>
-      <div></div>
+      <div className="flex flex-col items-center gap-2 ml-40">
+        <div className="flex items-center gap-6">
+          <Shuffle size={20} className="text-zinc-200" />
+          <SkipBack size={20} className="text-zinc-200"/>
+
+          <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+            <Play fill='bg-black' />
+          </button>    
+
+          <SkipForward size={20} className="text-zinc-200" />
+          <Repeat size={20} className="text-zinc-200"/>
+        </div>
+        <div className="flex items-center gap-2">
+          <span className="text-xs text-zinc-500">0:31</span>
+          <div className="h-1 rounded-full w-96 bg-zinc-600">
+            <div className="bg-zinc-200 w-40 h-1 rounded-full">
+            </div>
+          </div>
+          <span className="text-xs text-zinc-500">4:48</span>
+        </div>
+       </div>
+      <div className="flex items-center gap-4">
+        <PlaySquare size={20}/>
+        <Mic2 size={20}/>
+        <AlignJustify size={20} />
+        <MonitorSpeaker size={20} />
+        <div className="flex items-center gap-2">
+          <Volume2 size={20} />
+          <div className="h-1 rounded-full w-24 bg-zinc-600">
+            <div className="bg-zinc-200 w-16 h-1 rounded-full">
+            </div>
+          </div>
+        </div>
+        <Maximize2 size={20} />
+      </div>
     </footer>
   </div>
   )
